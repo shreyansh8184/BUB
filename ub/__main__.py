1a524926f82e58af2dfd1c01ee3e953d85585ad9
shreyansh8184 <54529384+shreyansh8184@users.noreply.github.com>
2020-10-04 19:35:33 +0530

    Update __main__.py

--- ub/__main__.py
+++ ub/__main__.py
@@ -1,5 +1,5 @@
 import sys
-from ub import app, LOGGER
+from ub import app, LOGGER, TOKEN
 from pyrogram import idle
 
 from ub.bot import instructions
@@ -7,6 +7,6 @@ from ub.bot import instructions
 if len(sys.argv) not in (1, 3, 4):
     quit(1)
 else:
-    app.start()
+    app.run_until_disconnected()
     LOGGER.info("Your bot is now online.")
-    idle()
+    app.start(bot_token=TOKEN)
