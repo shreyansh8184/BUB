6b707f79887312e958dd78b146a89c10778cf150
shreyansh8184 <54529384+shreyansh8184@users.noreply.github.com>
2020-10-04 15:20:35 +0530

    Update instructions.py

--- ub/bot/instructions.py
+++ ub/bot/instructions.py
@@ -1,7 +1,10 @@
 from ub import app
 from pyrogram import filters
+from pyrogram.types import InlineKeyboardMarkup
+from pyrogram.types import InlineKeyboardButton
+from pyrogram.types import CallbackQuery
 
-from ub.bot.strings import (START_TEXT, INFO, MONEY_DEPOSITED)
+from ub.bot.strings import (START_TEXT, INFO, MONEY_DEPOSITED, NOTED, DEPOSIT)
 
 @app.on_message(filters.command("start"))
 async def start(client, message):
@@ -9,9 +12,18 @@ async def start(client, message):
     user_id = message.from_user['id']
     mention = f"[{name}](tg://user?id={user_id})"
     await message.reply(START_TEXT)
-    await app.send_message(message.chat.id, INFO)
+    keyboard = InlineKeyboardMarkup([[InlineKeyboardButton(text="Deposit", callback_data=r"answer")]])
+    await app.send_message(message.chat.id, INFO, reply_markup=keyboard
 
 @app.on_message(filters.incoming & filters.photo)
 async def screenshot(client, message):
     await message.reply(MONEY_DEPOSITED)
+
+@app.on_callback_query()
+async def answer(client, callback_query):
+     await callback_query.edit_message_text(DEPOSIT)
+
+@app.on_message(filters.incoming & filters.text)
+async def msg(client, message):
+    await message.reply(NOTED)
 
