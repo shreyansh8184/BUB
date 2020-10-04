c1f8b97b612c0e6b304b8ac9f2f9add68b68c7ca
shreyansh8184 <54529384+shreyansh8184@users.noreply.github.com>
2020-10-04 20:07:41 +0530

    Update instructions.py

--- ub/bot/instructions.py
+++ ub/bot/instructions.py
@@ -6,6 +6,15 @@ from pyrogram.types import CallbackQuery
 
 from ub.bot.strings import (START_TEXT, INFO, MONEY_DEPOSITED, NOTED, DEPOSIT, RELEASED, REPORT, RELEASED, REPORTED)
 
+@app.on_callback_query()
+async def report(client, callback_query):
+          if callback_query.data == "answer":
+              await callback_query.edit_message_text(DEPOSIT)
+              return
+          else:
+            await callback_query.edit_message_text(REPORT)
+
+
 @app.on_message(filters.command("report"))
 async def rep(client, message):
     await message.reply(REPORTED)
@@ -20,16 +29,16 @@ async def start(client, message):
     user_id = message.from_user['id']
     mention = f"[{name}](tg://user?id={user_id})"
     await message.reply(START_TEXT)
-    keyboard = InlineKeyboardMarkup([[InlineKeyboardButton(text="Deposit", callback_data=r"answer")]])
+    keyboard = InlineKeyboardMarkup([[InlineKeyboardButton(text="Deposit", callback_data=r"answer"), InlineKeyboardButton(text="Report", callback_data=r"report")]])
     await app.send_message(message.chat.id, INFO, reply_markup=keyboard)
 
 @app.on_message(filters.incoming & filters.photo)
 async def screenshot(client, message):
     await message.reply(MONEY_DEPOSITED)
 
-@app.on_callback_query()
-async def answer(client, callback_query):
-          await callback_query.edit_message_text(DEPOSIT)
+#@app.on_callback_query()
+#async def answer(client, callback_query):
+          #await callback_query.edit_message_text(DEPOSIT)
 
 
 
