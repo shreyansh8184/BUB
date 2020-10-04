e6a6aa668233bcdc4a69b764dd05a8853e4c40d5
shreyansh8184 <54529384+shreyansh8184@users.noreply.github.com>
2020-10-04 14:52:07 +0530

    Update instructions.py

--- ub/bot/instructions.py
+++ ub/bot/instructions.py
@@ -1,15 +1,15 @@
 from ub import app
 from pyrogram import filters
 
-from ub.bot.strings import (PAYMENT_DETAILS, HOW, COD, FORMAT)
+from ub.bot.strings import (START_TEXT, INFO, MONEY_DEPOSITED)
 
 @app.on_message(filters.command("start"))
 async def start(client, message):
     name = message.from_user['first_name']
     user_id = message.from_user['id']
-    mention =  [{name}](tg://user?id={user_id})
+    mention = f"[{name}](tg://user?id={user_id})"
     await message.reply(START_TEXT)
-    await app.send_message(message.chat_id, INFO)
+    await app.send_message(message.chat.id, INFO)
 
 @app.on_message(filters.incoming & filters.photo)
 async def screenshot(client, message):
