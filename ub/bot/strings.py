1531160a2d6bdedd2be55369eeacc550368b2c1c
shreyansh8184 <54529384+shreyansh8184@users.noreply.github.com>
2020-10-04 15:26:45 +0530

    Update strings.py

--- ub/bot/strings.py
+++ ub/bot/strings.py
@@ -5,8 +5,24 @@ Hello {mention} I Hope You Are Fine. This is Telegram's Official Escrow Bot Made
 
 INFO = """
 
+If you are dealing with a Carder and you want to pay after you have received your product.
+
+You can deposit your money to us until your product has arrived.
+
+You can order us to release the payment after the product as arrived.
 """
 
 MONEY_DEPOSITED = """
 
+You have successfully deposited your money.
+
+It is in safe hands now ❤️
+
+Now Wait Your Product ❤️
+"""
+
+DEPOSIT = """
+To deposit your money, kindly send username/id of the person you are dealing with.
+
+Bank Details Are As Follows 👇
 """
